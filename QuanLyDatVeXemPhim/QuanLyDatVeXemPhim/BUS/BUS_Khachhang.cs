cd69bd6c8761ee827a69e1d65ccc46dad22a03f0
ngdgkhang <ngdgkhang@gmail.com>
2022-01-10 21:24:17 +0700

    Add Form Khachhang

---
+++ QuanLyDatVeXemPhim/QuanLyDatVeXemPhim/BUS/BUS_Khachhang.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuanLyDatVeXemPhim.DAO;
+
+namespace QuanLyDatVeXemPhim.BUS
+{
+    class BUS_Khachhang
+    {
+        DAO_Khachhang dAO_Khachhang;
+        public BUS_Khachhang()
+        {
+            dAO_Khachhang = new DAO_Khachhang();
+        }
+
+        public void ListKhachhang(DataGridView dg)
+        {
+            dg.DataSource = dAO_Khachhang.ListKhachhang();
+        }
+
+        public bool AddKhachhang(Member m)
+        {
+            bool kq = false;
+            try
+            {
+                dAO_Khachhang.addKhachhang(m);
+                kq = true;
+            }
+            catch 
+            {
+                
+            }
+            return true;
+        }    
+
+        public bool DeleteKhachhang(int id)
+        {
+            bool kq = false;
+            try
+            {
+                dAO_Khachhang.deleteKhachhang(id);
+                kq = true;
+            }
+            catch 
+            {
+
+            }
+            return true;
+        }
+        public bool UpdateKhachhang(Member m)
+        {
+
+            bool kq = false;
+            try
+            {
+                dAO_Khachhang.updateKhachhang(m);
+                kq = true;
+            }
+            catch
+            {
+
+            }
+            return true;
+        }
+    }
+}
--- QuanLyDatVeXemPhim/QuanLyDatVeXemPhim/Member.cs
+++ QuanLyDatVeXemPhim/QuanLyDatVeXemPhim/Member.cs
@@ -22,10 +22,8 @@ namespace QuanLyDatVeXemPhim
     
         public int idMember { get; set; }
         public string MemberName { get; set; }
-        public string Username { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
         public string Address { get; set; }
         public Nullable<System.DateTime> BirthDate { get; set; }
         public string Gender { get; set; }
